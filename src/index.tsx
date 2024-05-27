4f64febf13967ce2a55501c108ed1711087c5158
Markus Einan <markuseinan@hotmail.com>
2024-05-27 23:16:18 +0200

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-// @ts-expect-error - This is a valid call to ReactDOM
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
+  document.getElementById('root'),
 );
