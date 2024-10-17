4233536398a371bda3fad27396cf066bfff28c0e
Markus Einan <markuseinan@hotmail.com>
2024-10-18 00:37:45 +0200

    Add first version of page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './global.css';
 
 function App() {
   return (
     <div>
-      <h1>-Markus-</h1>
+      <div className="header">
+        <div>&#945;</div>
+        <div>&#946;</div>
+        <div>&#947;</div>
+        <div>&#948;</div>
+        <div>&#949;</div>
+        <div>&#950;</div>
+        <div>&#951;</div>
+      </div>
+      <div className="home-container">
+        <h1 className="h1">Markus</h1>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // @ts-expect-error - This is a valid call to ReactDOM
