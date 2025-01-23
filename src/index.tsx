8aa6f7e1836fb1c0762a4b90c66281f1f714ca64
Markus Einan <markuseinan@hotmail.com>
2025-01-23 22:11:44 +0100

    Add: settings.json and Fix: index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// @ts-expect-error - This is a valid call to ReactDOM
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+} else {
+  console.error('Root element not found');
+}
