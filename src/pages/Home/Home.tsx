40aed72551f6d7f327d5bfc179197761b5489732
Markus Einan <markuseinan@hotmail.com>
2025-02-14 23:56:06 +0100

    Add sections

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,15 +2,22 @@ import './Home.css';
 import Header from '../../components/Header';
 import profilbildeUrl from '../../assets/images/Profilbilde.jpg';
 import { Divider } from 'antd';
+
 function Home() {
   return (
-    <div>
+    <div className="container">
       <Header />
-      <div className="home-container">
-        <img src={profilbildeUrl} alt="Profilbilde" />
-        <h1 className="content">Web Developer</h1>
-        <Divider />
-      </div>
+      <section className="section home">
+        <div className="home-container">
+          <div className="imageContainer">
+            <img src={profilbildeUrl} alt="Profilbilde" />
+          </div>
+          <h1 className="content">Web Developer</h1>
+          <Divider />
+        </div>
+      </section>
+      <section className="section two">Section 2</section>
+      <section className="section three">Section 3</section>
     </div>
   );
 }
